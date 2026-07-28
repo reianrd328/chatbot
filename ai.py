455bb5fb3c71b987b6b2d44266f867d5f50fb5d4
richard ong <152252363+reianrd328@users.noreply.github.com>
2026-07-28 17:30:25 +0800

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,1 +1,28 @@
 
+from openai import OpenAI
+from config import Config
+
+client = OpenAI(
+    api_key=Config.API_KEY,
+    base_url=Config.BASE_URL
+)
+
+
+def ask_ai(message):
+
+    try:
+
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role": "user",
+                    "content": message
+                }
+            ]
+        )
+
+        return response.choices[0].message.content
+
+    except Exception as e:
+        return f"Error: {str(e)}"
