b6a91d18b1208ae328911e077acbeba5b221a4a8
richard ong <152252363+reianrd328@users.noreply.github.com>
2026-07-31 15:25:47 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
     __tablename__ = "users"
