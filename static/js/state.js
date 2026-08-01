8f8fd93576ceacc4c18ee1d3de33bd559430e428
richard ong <152252363+reianrd328@users.noreply.github.com>
2026-08-01 14:32:06 +0800

    Update state.js

--- static/js/state.js
+++ static/js/state.js
@@ -1,1 +1,25 @@
+/* ==========================================
+   LYRCH AI STATE
+========================================== */
 
+"use strict";
+
+const State = {
+
+    // Chat
+    currentChatId: null,
+
+    chatting: false,
+
+    loading: false,
+
+    // Future Features
+    model: "OpenAI",
+
+    streaming: false,
+
+    uploadedFiles: []
+
+};
+
+window.State = State;
