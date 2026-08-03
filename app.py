c838ecb7be54c1a528e8149976aa519db5a3628a
richard ong <152252363+reianrd328@users.noreply.github.com>
2026-08-03 14:56:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,21 @@ from ai import ask_ai
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+app.register_blueprint(auth)
 # ==========================
 # Get One Chat
 # ==========================
