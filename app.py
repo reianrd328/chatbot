af2b7a1d0108880939cd2173d1d77db0b61b4ac1
richard ong <152252363+reianrd328@users.noreply.github.com>
2026-08-03 14:50:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+from flask import Flask, redirect, render_template, request, jsonify
+from flask_login import LoginManager, login_required, current_user
+
+from config import Config
+from models import db, User, Chat, Message
+from auth import auth
+from ai import ask_ai
+
+app = Flask(__name__)
+app.config.from_object(Config)
 # ==========================
 # Get One Chat
 # ==========================
@@ -46,10 +56,6 @@ def logout_session():
 
     return "", 204
 
-
-# ==========================
-# Run App
-# ==========================
 
 if __name__ == "__main__":
     app.run(debug=True)
