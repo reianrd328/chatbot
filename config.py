b4d10df77de66290fde173d6c3aa74c836960050
richard ong <152252363+reianrd328@users.noreply.github.com>
2026-08-04 16:42:30 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,28 +1,32 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    # Flask
+    SECRET_KEY = os.getenv("SECRET_KEY", "change-this-secret")
 
+    # Database
     DB_HOST = os.getenv("DB_HOST")
-    DB_PORT = os.getenv("DB_PORT")
+    DB_PORT = os.getenv("DB_PORT", "4000")
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_NAME = os.getenv("DB_NAME")
+    DB_NAME = os.getenv("DB_NAME", "chatbot")
 
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
         f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+        "?ssl_verify_cert=false"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        "pool_pre_ping": True,
-        "pool_recycle": 300,
-        "connect_args": {
-            "ssl": {}
-        }
-    }
-
+    # AI
     API_KEY = os.getenv("API_KEY")
-    BASE_URL = os.getenv("BASE_URL")
+    BASE_URL = os.getenv(
+        "BASE_URL",
+        "https://router.bynara.id/v1"
+    )
+
+    MODEL = os.getenv(
+        "MODEL",
+        "agnes-2.5-flash"
+    )
