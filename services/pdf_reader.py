ae432e96c3f42b9f01d452f76fcca2579205aa12
richard ong <152252363+reianrd328@users.noreply.github.com>
2026-08-05 20:38:10 +0800

    Update pdf_reader.py

--- services/pdf_reader.py
+++ services/pdf_reader.py
@@ -1,1 +1,12 @@
+from pypdf import PdfReader
 
+def read_pdf(path):
+
+    reader = PdfReader(path)
+
+    text = ""
+
+    for page in reader.pages:
+        text += page.extract_text() or ""
+
+    return text
